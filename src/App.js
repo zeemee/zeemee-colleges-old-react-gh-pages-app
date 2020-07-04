f2439ae6b5a8f1b3c970258e6e91fcbf64cb12f9
Ben Wiseley <wiseleyb@gmail.com>
2020-07-04 13:27:10 -0700

    Add extremely basic api call case studies example

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import CaseStudies from './components/CaseStudies';
 
 export default function App() {
   return (
@@ -16,7 +17,7 @@ export default function App() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/caseStudies">Case Studies</Link>
             </li>
             <li>
               <Link to="/users">Users</Link>
@@ -27,8 +28,8 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/caseStudies">
+            <CaseStudiesPage />
           </Route>
           <Route path="/users">
             <Users />
@@ -46,8 +47,8 @@ function Home() {
   return <h2>Home</h2>;
 }
 
-function About() {
-  return <h2>About</h2>;
+function CaseStudiesPage() {
+  return <CaseStudies/>;
 }
 
 function Users() {
---
+++ src/components/CaseStudies.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+class CaseStudies extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://www.zaptack.com/api/poja/colleges/case_studies.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+
+export default CaseStudies;
