918b0240196ba769cc27e2a5f37c615cb7598a73
Ben Wiseley <wiseleyb@gmail.com>
2020-07-04 06:38:10 -0700

    Updates

---
+++ precache-manifest.3686ee7cb206f0c4295003f87894354c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf0ef653308f489f3feba5ca47172de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61865639b088e829d7ff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7f1010647a9e48e2f15",
+    "url": "/static/js/2.787578cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.787578cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61865639b088e829d7ff",
+    "url": "/static/js/main.98b2ed7f.chunk.js"
+  },
+  {
+    "revision": "8dcf5bf255e614680dcd",
+    "url": "/static/js/runtime-main.48d3c3e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
